daac11f99bcecb9943b71e360e18b1fafb48bfd2
Bruno Albuquerque Carneiro <bruno.carneiro@wisereducacao.com>
2022-08-13 21:39:30 -0300

    fix: ajuste no texto da bio

--- src/components/NavBar/navBar.tsx
+++ src/components/NavBar/navBar.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import avatar from '../../assets/foto.jpg';
 import { Container, TextContainer } from './styles';
-import Image from 'next/image'
 
-export interface ILogo {
-  //   image: HTMLImageElement;
-
-}
 
 function NavBar() {
   return (
@@ -16,33 +10,6 @@ function NavBar() {
           <h2>Bruno Albuquerque Carneiro </h2>
         </TextContainer>
       </div>
-{/* <div style={{flex:1}}></div>
-      <div style={{flex:1, justifyContent:'space-evenly',display:'flex'}}>
-
-      <div>
-       <h3>Inicio</h3>
-      </div>
-      <div>
-         <h3>Tecnologias</h3>
-      </div>
-      <div>
-         <h3>Projetos</h3>
-      </div>
-      <div>
-         <h3>Tragetoria</h3>
-      </div>
-      <div>
-         <h3>Contato</h3>
-      </div>
-      </div> */}
-      {/* <div style={{margin:40}}>
-        <TextContainer>
-          <h2>Bruno Albuquerque</h2>
-        </TextContainer>
-      
-      </div> */}
-    
-      
     </Container>
   );
 }
