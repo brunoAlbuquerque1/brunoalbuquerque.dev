b266851a99867d81c7bf5bd62cdfd38e279f6fea
Bruno Albuquerque Carneiro <bruno.carneiro@wisereducacao.com>
2022-08-13 23:36:22 -0300

    fix: teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,20 @@
 import { useEffect } from 'react';
-import Aos from 'aos';
-import NavBar from '../components/NavBar/navbar';
-import About from  '../components/About/about';
+
 
 // import Sidebar from '../components/Sidebar';
 //  import Tecnology from '../components/Tecnology/Tecnology';
 //  import Projects from '../components/Projects/projects';
 //  import Conhecimento from '../components/Conhecimento';
 
- import 'aos/dist/aos.css';
 
 
 export default function Home() {
-  useEffect(() => {
-    Aos.init({ duration:1500 });
-  }, []);
+
 
   return (
 <>
-     
-      <NavBar /> 
-      <main className="container">
-       <About/>
-    
-  
-      </main>
+<p>aaaaaaaaaaaaaaaaaaaaa</p>
+      
   </>
   );
 }
