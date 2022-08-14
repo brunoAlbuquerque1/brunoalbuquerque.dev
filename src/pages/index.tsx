da4b5006f6ed046e00b498411d5ca4fa3e1f5db9
Bruno Albuquerque Carneiro <bruno.carneiro@wisereducacao.com>
2022-08-13 23:41:01 -0300

    fix: ajuste no texto da bio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,30 @@
 import { useEffect } from 'react';
+import Aos from 'aos';
+import NavBar from '../components/NavBar/navbar';
+// import About from  '../components/About/about';
 
-
-// import Sidebar from '../components/Sidebar';
-//  import Tecnology from '../components/Tecnology/Tecnology';
-//  import Projects from '../components/Projects/projects';
-//  import Conhecimento from '../components/Conhecimento';
+// // import Sidebar from '../components/Sidebar';
+// //  import Tecnology from '../components/Tecnology/Tecnology';
+// //  import Projects from '../components/Projects/projects';
+// //  import Conhecimento from '../components/Conhecimento';
 
  import 'aos/dist/aos.css';
 
 
 export default function Home() {
-
+  useEffect(() => {
+    Aos.init({ duration:1500 });
+  }, []);
 
   return (
 <>
-<p>aaaaaaaaaaaaaaaaaaaaa</p>
-      
+     
+      <NavBar /> 
+      <main className="container">
+       {/* <About/> */}
+    
+  
+      </main>
   </>
   );
 }
