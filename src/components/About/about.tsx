0cce03b759a19f26f55782c82b5318a571aa1766
Bruno Albuquerque Carneiro <bruno.carneiro@wisereducacao.com>
2022-08-13 21:28:28 -0300

    fix: ajuste no texto da bio

--- src/components/About/about.tsx
+++ src/components/About/about.tsx
@@ -4,16 +4,11 @@ import avatar from '../../assets/foto.jpg';
 
 function About() {
   return (
-    // <Container data-aos="fade-down">
     <Container >
       <div >
         <TextContainer>
           <h3>Desenvolvedor <br/> Front-End/Mobile</h3>
-          <h4>Desenvolvedor Front-End e Mobile por volta de 
-          uns 5 anos trabalhando em diversos projetos des do 
-          "Tipo Uber" ate o "Tipo Ifood" em diversars areas de atuação,
-          e sempre estou atras de evoluir tanto minha hard Skill e Soft Skill 
-          </h4>
+          <h4> Desenvolvedor Front-End e Mobile por volta de uns 5 anos trabalhando em diversos projetos des do  "Tipo Uber" ate o "Tipo Ifood" em diversars areas de atuação </h4>
         </TextContainer>
       </div>
       <div style={{justifyContent:'center',display:'flex'}}>
