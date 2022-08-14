6b2fee7582f87ba414bb0352d1229a6bfe267a15
Bruno Albuquerque Carneiro <bruno.carneiro@wisereducacao.com>
2022-08-14 20:34:02 -0300

    feat: esperiencia

--- src/components/Experiences/experiences.tsx
+++ src/components/Experiences/experiences.tsx
@@ -9,7 +9,12 @@ function Tecnology() {
     <Container>
         <TitleSection title="Minha trajetória" />
       
-      <Item data='123' nameCompany='123'  office='123' description='123'/>
+      <Item data='Julho - 2022' nameCompany='Wiser Educação '  office='Senior Frontend Developer' description='Desenvolvedor Front-End ReactJs e Next.Js atuando no time de arquitetura melhorando a performace dos projetos da wiser'/>
+      <Item data='Maio - 2022' nameCompany='Mouts TI'  office='Senior Frontend Developer' description='Desenvolvedor Front-End ReactJS Alocado no Grupo Ab Inbev'/>
+      <Item data='Março - 2020' nameCompany='MagIT Partner'  office='Software Developer' description='Desenvolvedor Front End e mobile nas tecnologias React-native, React.js, '/>
+      <Item data='Maio - 2019' nameCompany='Dart Digital'  office='Developer Mobile' description='Desenvolvedor Mobile com React-Native'/>
+      <Item data='Julho - 2022' nameCompany='Adekz'  office='Developer Mobile' description='Desenvolvedor Mobile com React-Native'/>
+      <Item data='Julho - 2022' nameCompany='Mouts TI'  office='Developer Mobile' description='Desenvolvedor Mobile, Front-End'/>
 
     </Container>
     
